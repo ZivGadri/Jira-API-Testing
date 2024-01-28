495a2cf8a9dd34a9e0f6f71cf61272d81d3af951
ziv <ziv.automation@gmail.com>
2024-01-28 08:18:17 +0200

    Initial commit - basic project infrastructure

---
+++ src/main/java/genericApiRequests.java
@@ -0,0 +1,40 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import static io.restassured.RestAssured.*;
+
+public class genericApiRequests {
+
+    public static Response post() {
+        RestAssured.baseURI = Constants.jiraBaseUrl;
+        Response response =
+                given().header("Content-Type","application/json")
+                        .body("").when().post();
+        return response;
+    }
+
+    public static Response get() {
+        RestAssured.baseURI = Constants.jiraBaseUrl;
+        Response response =
+                given().header("Content-Type","application/json")
+                        .body("").when().get();
+        return response;
+    }
+
+    public static Response put() {
+        RestAssured.baseURI = Constants.jiraBaseUrl;
+        Response response =
+                given().header("Content-Type","application/json")
+                        .body("").when().put();
+        return response;
+    }
+
+    public static Response delete() {
+        RestAssured.baseURI = Constants.jiraBaseUrl;
+        Response response =
+                given().header("Content-Type","application/json")
+                        .body("").when().delete();
+        return response;
+    }
+
+
+}
