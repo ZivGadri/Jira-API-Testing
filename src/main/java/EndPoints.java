068e6b3ed60073025ec25b9b42ea7242b4708af3
Ziv Gadri <ziv.automation@gmail.com>
2024-02-16 13:18:50 +0200

    Setting up the end points

--- src/main/java/EndPoints.java
+++ src/main/java/EndPoints.java
@@ -1,4 +1,12 @@
 public class EndPoints {
 
-    public static final String createSession = "rest/auth/1/session";
+    public static final String createSession = "/rest/auth/1/session";
+    public static final String createProject = "/rest/api/2/project";
+    public static final String createIssue = "/rest/api/2/issue";
+    public static final String addComment = "/rest/api/2/issue/{issueIdOrKey}/comment";
+    public static final String updateComment = "/rest/api/2/issue/{issueIdOrKey}/comment/{id}";
+    public static final String deleteComment = "/rest/api/2/issue/{issueIdOrKey}/comment/{id}";
+    public static final String deleteIssue = "/rest/api/2/issue/{issueIdOrKey}";
+    public static final String deleteProject = "/rest/api/2/project/{projectIdOrKey}";
+
 }
