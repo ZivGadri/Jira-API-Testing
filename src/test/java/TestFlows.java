73da3da0d07a7e72ef386f8e37151fd6083daffc
Ziv Gadri <ziv.automation@gmail.com>
2024-04-04 08:22:34 +0300

    Delete Comment flow

--- src/test/java/TestFlows.java
+++ src/test/java/TestFlows.java
@@ -1,27 +1,38 @@
-import apiManager.models.Comment;
+import apiManager.ApiHelper;
 
 public class TestFlows extends BaseTests {
 
+    public TestFlows(ApiHelper apiHelper) {
+        super(apiHelper);
+    }
+
     public void testCreateNewProjectAndAssertWithUI() {
         testProject = apiHelper.createNewProject(buildProjectObject());
-        threadSleepLog(3);
+        threadSleepLog(3, "right after creating a new project");
         softAssert.assertEquals(testProject.getName(), PROJECT_NAME);
         softAssert.assertAll();
     }
 
     public void testCreatingANewIssueAndAssertUsingUI() {
         testIssue = apiHelper.createNewIssue(buildIssueObject(testProject));
-        threadSleepLog(3);
+        threadSleepLog(3, "right after creating a new issue");
 
     }
 
     public void testAddingACommentToNewIssue() {
-        testComment = apiHelper.AddComment(createCommentObject(testCommentText), testIssue);
-        threadSleepLog(3);
+        testComment = apiHelper.addComment(createCommentObject(testCommentText), testIssue);
+        threadSleepLog(3, "right after adding a comment");
     }
 
     public void testUpdatingAComment() {
         testComment.setBody(updatedTestCommentText);
-        Comment updatedTestComment = apiHelper.UpdateComment(testComment, testIssue);
+        testComment = apiHelper.updateComment(testComment, testIssue);
+        threadSleepLog(3, "right after updating a comment");
+    }
+
+    public void testDeletingACommentFromIssue() {
+        apiHelper.deleteComment(testComment, testIssue);
+        threadSleepLog(3, "right after deleting a comment");
+        softAssert.assertEquals(getNumberOfCommentsForIssue(testIssue), 0);
     }
 }
